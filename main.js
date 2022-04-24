802693e09ff3bed6a521f7713db9b37af64dfd19
Eliéser <elieser.andrade@fatecitapetininga.edu.br>
2022-04-23 22:44:25 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.js";
+
+console.log(mostraIdade("Eliéser", 21));
+console.log(mostraCidade("Eliéser", "Itapetininga"));
+console.log(mostraHobby("Eliéser", "Programar"));
